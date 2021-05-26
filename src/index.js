a0daa0be4d34e76d4fecf074b3482598f33cc886
Mo Ghanbari <mo.ghanbari@icloud.com>
2021-05-25 22:29:29 -0400

    Added Tailwind.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'assets/tailwind.css'
 import Routes from './Routes'
-import App from './App';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App><Routes /></App>
+    <App>
+      <Routes />
+    </App>
   </React.StrictMode>,
   document.getElementById('root')
 )
