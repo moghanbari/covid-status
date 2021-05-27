add7f11cac6f947c82fd8d0cf15efcdff5d3dedb
Mo Ghanbari <mo.ghanbari@icloud.com>
2021-05-27 15:55:12 -0400

    Added about page content. Accurate data will be added soon

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const About = () => {
-  return (
-    <div>
-      <h1>About page</h1>
+const About = () => (
+  <div className='grid md:grid-cols-1'>
+    <div className='p-10 xs:p-5 text-center rounded mb-3 mt-3'>
+      <p>
+        Just a simple experimental React project. API by{' '}
+        <a class='text-blue-400' href='https://covid19api.com' target='_blank' rel='noreferrer'>
+          covid19api.com
+        </a>
+        . You can see the project code in{' '}
+        <a class='text-blue-400' href='https://github.com' target='_blank' rel='noreferrer'>
+          here
+        </a>
+        . Also you can checkout my other projects at{' '}
+        <a class='text-blue-400' href='https://mocode.ca' target='_blank' rel='noreferrer'>
+          mocodes.ca
+        </a>
+      </p>
     </div>
-  )
-}
+  </div>
+)
 
 export default About
